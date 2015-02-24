091363bae7407d5e62527cc20cea9f70d334c6bc
Alexey Kodanev <alexey.kodanev@oracle.com>
2015-02-24 19:58:58 +0300

    r-mm/perseptron: print weights
    Signed-off-by: Alexey Kodanev <alexey.kodanev@oracle.com>

--- r-mm/perseptron.R
+++ r-mm/perseptron.R
@@ -107,21 +107,21 @@ ln_back_prop_fn2 = function()
   
   learn = 1;
   steps = 1;
-  while (steps < 100) {
+  while (1) {
     steps = steps + 1;
     
     for (i in seq(1, n_cnt)) {
       s[i] = b[i] + w[2,i, ] %*% x;
     }
   
-    y = act_bsigmoid_fn(s);
+    y = act_sigmoid_fn(s);
             
     if (steps %% 3 == 0) {
       print("----------------");
       print("x = "); print(x);
       print("y = "); print(y);
-      
-      res = readline('Continue or run example (y, n, num(1..3))?');
+
+      res = readline('Continue or run example (y, n, num(1..3), p(plot weights))?');
       learn = 1;
       if (res == "n") {
         break;
@@ -134,35 +134,42 @@ ln_back_prop_fn2 = function()
       } else if (res == "3") {
         x = ex_x[3,];
         d = ex_d[3,];
+      } else if (res == "p") {
+        plot_weights(w[3,,]);
       } else {
         learn = 0;
         x = c(1, 1, 1, 1);
       }
     }
-    
+
     if (!learn)
       next;
     # Learning...
     # adjust weights if needed
     n_err = (d - y); 
     #print("n_err = "); print(n_err);
-  
+
     a = 0.01;
     nu = 0.45;
-  
+
     for (i in seq(1, n_cnt)) {
       w[3,i,] = w[2,i,] + a * w[1,i,] + (nu * n_err[i]) * x;
     }
-  
+
     w[1,,] = w[2,,];
     w[2,,] = w[3,,];
-    
+
     # new weights
-    #print("w[3,i] = "); print(w[3,,]);        
+    print("w[3,i] = "); print(w[3,,]);
   }
 
 }
 
+plot_weights = function(w)
+{
+  x = seq(1, length(w[1,]));
+  plot_xy2(x, min(x), max(x), "x", w, "weights");
+}
 
 multilayer_perceptron = function()
 {
@@ -203,3 +210,36 @@ plot_xy = function(x, min_x, max_x, x_str, y, min_y, max_y, y_str)
   
   par(op);#- reset to default
 }
+
+plot_xy2 = function(x, min_x, max_x, x_str, y, y_str)
+{
+  cols = c("red", "blue", "black", "green");
+  col_grid = "#5F5F5F";
+
+  plot_count = 1;
+  #bottom, left, top, right
+  op<-par(mfrow=c(plot_count,1), mar=c(2,5,1,1), xaxs='i',lab=c(12, 5, 7));
+
+  # setting up coord. sys.
+  l = length(y[1,]);
+  max_x = max(x) + 1;
+  min_x = min(x) - 1;
+  if (l == 1) {
+    step = 0;
+  } else {
+    step = abs(max(y) - min(y)) / (l - 1);
+    step_x = abs(max_x - min_x) / (l - 1);
+  }
+  print(min(y));print(max(y));
+  plot(seq(min_x, max_x, step_x), seq(min(y), max(y), step),
+    type ='n', ylab=y_str);
+  grid(lwd = 1, col = col_grid);
+
+  #-------------------------------------------------
+  for (i in seq(1, length(y[,1]))) {
+     points(x, y[i,], type="p", col = cols[i]);
+  }
+
+  par(op); # - reset to default
+}
+
