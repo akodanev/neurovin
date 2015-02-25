2764a3c9596d55036cc887482ec84758454bb12e
Alexey Kodanev <akodanev@gmail.com>
2015-02-26 01:37:14 +0300

    r-mm/perceptron.R: restructure code, use cat() instead of print()
    Signed-off-by: Alexey Kodanev <akodanev@gmail.com>

--- r-mm/perceptron.R
+++ r-mm/perceptron.R
@@ -47,30 +47,11 @@ act_threshold_deriv_fn = function(x, k = 1)
   return (0);
 }
 
-# Learning Algorithm
-# ========
 
-# 1. Back-propagation learning
-# ----------------------------
-ln_back_prop_fn = function()
-{
-  ln_speed = 0.1;
-  ln_moment = 0.1;
-  
-  in_cnt = 2; 
-  neurons = c(2, 1);
-  layers_cnt = length(neurons);
-  
-  n_err = matrix(0, nrow = layers_cnt, ncol = max(neurons));
-  print(n_err);
-  w = array(0, dim = c(layers_cnt, max(neurons), in_cnt));
-  print(w);
-}
+# 1. Single-layer artificial neural network with learning
+# -------------------------------------------------------
 
-# 2. Back-propagation learning - 1 layer
-# ----------------------------
-
-one_layer_net_calc = function(b, w, x, n_cnt)
+single_net_calc = function(b, w, x, n_cnt, k)
 {
   s = rep(0, n_cnt);
 
@@ -78,202 +59,122 @@ one_layer_net_calc = function(b, w, x, n_cnt)
     s[i] = b[i] + w[2,i, ] %*% x;
   }
 
-  y = act_sigmoid_fn(s);
+  #y = act_threshold_fn(s);
+  y = act_sigmoid_fn(s, k);
 
   return (y);
 }
 
-one_layer_net_learn = function(w, y, d, x, n_cnt)
+single_net_learn = function(b, w, y, d, x, n_cnt)
 {
-  # Learning...  
-  a = 0.001;
+  # Learning...
+  
+  # learning rate 0.1..0.9
   lr = 1;
-  # adjust weights if needed
-  # lr is learning rate 0.1..0.9
-
+  
   # delta rule, h is the weighted sum of the neuron's inputs
-  h = rep(0, n_cnt);
-  b = rep(0, n_cnt);
-# for (i in seq(1, n_cnt)) {
-#    h[i] = b[i] + w[2,i, ] %*% x;
-#  }
-
-  f_deriv = 1; # act_sigmoid_deriv_fn(h);
-  n_err = (d - y) * f_deriv;
-  #print("---");
-  #print(h);
-  #print(d - y);
-  #print(n_err);
-
+  # h = rep(0, n_cnt);
+  
+  n_err = lr * (d - y);
+  
   for (i in seq(1, n_cnt)) {
-    w[3,i,] = w[2,i,] + a * w[1,i,] + n_err[i] * x;
+    w[2,i,] = w[2,i,] + n_err[i] * x;
   }
-
-  w[1,,] = w[2,,];
-  w[2,,] = w[3,,];
 
   return (w);
 }
 
-one_layer_net = function()
+single_net = function()
 {
   ret = ex_data_pic16x16_for_learn();
   #ret = ex_data_x4d3();
   ex_x = ret[[1]]; ex_d = ret[[2]];
   rm(ret);
-
+  
   # inputs
   i_cnt = length(ex_x[1,]);
-  print("inputs number is:"); print(i_cnt);
+  
   # neurons in the layer
   n_cnt = 3;
-
-  b = rep(0, n_cnt);
+  
+  fn_k = 1;
+  
+  b = seq(0, n_cnt);
   # desired output, for learning
   d = rep(0, n_cnt);
-
+  
   # weights to remember
   w_mem_step = 3;
-
+  
   # initialize weights
   w = array(0, dim = c(w_mem_step, n_cnt, i_cnt));
-
-
+  
   # run auto training
   i = 0;
-  while (i < 500) {
-
+  while (i < 20) {
+    
     id = (i %% n_cnt) + 1;
     x = ex_x[id,];
     d = ex_d[id,];
-
-    y = one_layer_net_calc(b, w, x, n_cnt);
-    w = one_layer_net_learn(w, y, d, x, n_cnt);
-
+    
+    y = single_net_calc(b, w, x, n_cnt, fn_k);
+    w = single_net_learn(b, w, y, d, x, n_cnt);
+    
     i = i + 1;
   }
-
-  plot_weights(w[3,,]);
-
-  # test run
-  xv = ex_data_pic16x16_for_test();
-  #xv = ex_data_x4();
-  print("num of examples: ");print(length(xv[,1]));
-
-  for (i in seq(1, length(xv[,1]))) {
-    x = xv[i,];
-    y = one_layer_net_calc(b, w, x, n_cnt);
-
-    print(" "); print("----");print(i)
-    # print("   |-->inputs:"); print(xv[i,]);
-    print("   |-->outputs:"); print(round(y, 5));
-  }
-
-}
-
-ex_data_x4 = function()
-{
-  ex_x = array(0, dim = c(5,4));
-  ex_x[1,1] = 0; ex_x[1,2] = 1; ex_x[1,3] = 1; ex_x[1,4] = 0;
-  ex_x[2,1] = 1; ex_x[2,2] = 0; ex_x[2,3] = 0; ex_x[2,4] = 1;
-  ex_x[3,1] = 1; ex_x[3,2] = 1; ex_x[3,3] = 0; ex_x[3,4] = 1;
-  ex_x[4,1] = 1; ex_x[4,2] = 1; ex_x[4,3] = 1; ex_x[4,4] = 1;
-  ex_x[5,1] = 0; ex_x[5,2] = 1; ex_x[5,3] = 1; ex_x[5,4] = 1;
   
-  return (ex_x);
+  plot_weights(w[2,,]);
+  
+  # test run
+  for (i in seq(1, 5)) {
+    
+    cat("\nTest #", i, ": ");
+    
+    x = ex_data_pic16x16_for_test(i);
+    
+    y = single_net_calc(b, w, x, n_cnt, fn_k);
+    
+    analyze_results(y);
+  }
+  
 }
 
-alg_matrix_to_spiral = function(x, m, rs = 0, re = 0, cs = 0, ce = 0, id = 1,
-                                        rev = 0, row = 0)
+analyze_results = function(y)
 {
-  if (rs > re && cs > ce) {
-    return (x);
-  }
-
-  if (row == 0) {
-    if (rev == 0) {
-      for (i in seq(cs, ce)) {
-        x[id] = m[rs,i];
-        id = id + 1;
-      }
-      if (id > length(x)) {
-        return (x);
-      }
+  alp = c('A', 'B', 'C');
+  id = -1;
+  uncertanty = -1;
+  
+  for (i in seq(1, length(y))) {
+    if (y[i] >= 0.9) {
+      id = i;
+      uncertanty = uncertanty + 1;
     }
-    rs = rs + 1;
-    row = 1;
-    rev = 0;
   }
-
-  if (row == 1) {
-    if (rev == 0) {
-        for (i in seq(rs, re)) {
-          x[id] = m[i,ce];
-          id = id + 1;
-        }
-        if (id > length(x)) {
-          return (x);
-        }
-    }
-    ce = ce - 1;
-    row = 0;
-    rev = 1;
+  
+  if (id >= 0 && uncertanty == 0) {
+      cat(">OK, I know it, this is letter", alp[id], "\n");
+      return (0);
   }
-
-  if (row == 0) {
-    if (rev == 1) {
-        for (i in seq(ce, cs)) {
-          x[id] = m[re,i];
-          id = id + 1;
-        }
-        if (id > length(x)) {
-          return (x);
-        }
-    }
-    re = re - 1;
-    row = 1;
-    rev = 1;
-  }
-
-  if (row == 1) {
-    if (rev == 1) {
-        for (i in seq(re, rs)) {
-          x[id] = m[i,cs];
-          id = id + 1;
-        }
-        if (id > length(x)) {
-          return (x);
-        }
-    }
-    cs = cs + 1;
-    row = 0;
-    rev = 0;
-  }
-
-  x = alg_matrix_to_spiral(x, m, rs, re, cs, ce, id, rev, row);
-
-  return (x);
+  
+  cat(">Hmm, not exactly sure what it is", round(y, 5), "\n");
 }
 
 ex_data_pic16x16_for_learn = function()
 {
   library(png);
 
-  #tmp_x = array(0, dim = c(16,16));
-
+  xv = rep(0, 256);
   ex_x = array(0, dim = c(3,256));
   ex_d = array(0, dim = c(3,3));
 
   id = c("img16x16/A.png", "img16x16/B.png", "img16x16/C.png");
 
   for (i in seq(1, 3)) {
-    img = readPNG(id[i]);
-    #tmp_x = as.vector(t(img[,,4]));
-    #len2 = length(tmp_x) / 2;
-    #for (k in seq(1, len2)) {
-    #  ex_x[i,k] = tmp_x[len2 - k]
-    #}
-    ex_x[i,] = as.vector(t(img[,,4]));
+    img = readPNG(id[i]);    
+    #tmp_x = t(img[,,4]);
+    #ex_x[i,] = alg_matrix_to_spiral(xv, tmp_x, 1, 16, 1, 16);
+    ex_x[i,] = as.vector(t(img[,,4]));        
   }
 
   ex_d[1,1] = 1; ex_d[1,2] = 0; ex_d[1,3] = 0; # A
@@ -283,71 +184,23 @@ ex_data_pic16x16_for_learn = function()
   return (list(ex_x, ex_d));
 }
 
-ex_data_pic16x16_for_test = function(i = 0)
+ex_data_pic16x16_for_test = function(i)
 {
   library(png);
-  ex_x = array(0, dim = c(5,256));
-
+  
+  xv = rep(0, 256);
+  
   id = c("img16x16/A0.png", "img16x16/B0.png", "img16x16/C0.png", "img16x16/D0.png",
          "img16x16/E0.png");
 
-  for (i in seq(1, 5)) {
-    img = readPNG(id[i]);
-    ex_x[i,] = as.vector(t(img[,,4]))
-  }
+  img = readPNG(id[i]);
+    #tmp_x = t(img[,,4]);
+    #ex_x[i,] = alg_matrix_to_spiral(xv, tmp_x, 1, 16, 1, 16);
+  xv = as.vector(t(img[,,4]))
 
-  return (ex_x);
-}
-
-
-ex_data_x4d3 = function()
-{
-  ex_x = array(0, dim = c(3,4));
-  ex_d = array(0, dim = c(3,3));  
-    
-  #slash
-  ex_x[1,1] = 0; ex_x[1,2] = 1; ex_x[1,3] = 1; ex_x[1,4] = 0;
-  ex_d[1,1] = 1; ex_d[1,2] = 0; ex_d[1,3] = 0;
+  cat("read file", id[i], "\n");
   
-  # backslash
-  ex_x[2,1] = 1; ex_x[2,2] = 0; ex_x[2,3] = 0; ex_x[2,4] = 1;
-  ex_d[2,1] = 0; ex_d[2,2] = 1; ex_d[2,3] = 0;
-  
-  # ar
-  ex_x[3,1] = 1; ex_x[3,2] = 1; ex_x[3,3] = 0; ex_x[3,4] = 1;
-  ex_d[3,1] = 0; ex_d[3,2] = 0; ex_d[3,3] = 1;
-  
-  return (list(ex_x, ex_d));
-}
-
-check_point = function()
-{        
-#   if (steps %% 3 != 0)
-#     return;
-#   
-#   print("----------------");
-#   print("x = "); print(x);
-#   print("y = "); print(y);
-# 
-#   res = readline('Continue or run example (y, n, num(1..3), p(plot weights))?');
-#   learn = 1;
-#   if (res == "n") {
-#     return;
-#   } else if (res == "1") {
-#     x = ex_x[1,];
-#     d = ex_d[1,];
-#   } else if (res == "2") {
-#     x = ex_x[2,];
-#     d = ex_d[2,];
-#   } else if (res == "3") {
-#     x = ex_x[3,];
-#     d = ex_d[3,];
-#   } else if (res == "p") {
-#     plot_weights(w[3,,]);
-#   } else {
-#     learn = 0;
-#     x = c(1, 1, 1, 1);
-#   }
+  return (xv);
 }
 
 plot_weights = function(w)
@@ -356,7 +209,7 @@ plot_weights = function(w)
   plot_xy2(x, min(x), max(x), "x", w, "weights");
 }
 
-multilayer_perceptron = function()
+plot_functions = function()
 {
   rm(list=ls());
   
@@ -428,3 +281,144 @@ plot_xy2 = function(x, min_x, max_x, x_str, y, y_str)
   par(op); # - reset to default
 }
 
+# Other algorithms
+# ==================
+
+alg_matrix_to_spiral = function(x, m, rs = 0, re = 0, cs = 0, ce = 0, id = 1,
+                                rev = 0, row = 0)
+{
+  if (rs > re && cs > ce) {
+    return (x);
+  }
+  
+  if (row == 0) {
+    if (rev == 0) {
+      for (i in seq(cs, ce)) {
+        x[id] = m[rs,i];
+        id = id + 1;
+      }
+      if (id > length(x)) {
+        return (x);
+      }
+    }
+    rs = rs + 1;
+    row = 1;
+    rev = 0;
+  }
+  
+  if (row == 1) {
+    if (rev == 0) {
+      for (i in seq(rs, re)) {
+        x[id] = m[i,ce];
+        id = id + 1;
+      }
+      if (id > length(x)) {
+        return (x);
+      }
+    }
+    ce = ce - 1;
+    row = 0;
+    rev = 1;
+  }
+  
+  if (row == 0) {
+    if (rev == 1) {
+      for (i in seq(ce, cs)) {
+        x[id] = m[re,i];
+        id = id + 1;
+      }
+      if (id > length(x)) {
+        return (x);
+      }
+    }
+    re = re - 1;
+    row = 1;
+    rev = 1;
+  }
+  
+  if (row == 1) {
+    if (rev == 1) {
+      for (i in seq(re, rs)) {
+        x[id] = m[i,cs];
+        id = id + 1;
+      }
+      if (id > length(x)) {
+        return (x);
+      }
+    }
+    cs = cs + 1;
+    row = 0;
+    rev = 0;
+  }
+  
+  x = alg_matrix_to_spiral(x, m, rs, re, cs, ce, id, rev, row);
+  
+  return (x);
+}
+
+
+
+# Functions that are no longer used (will be removed)
+# ===================================================
+
+# ex_data_x4d3 = function()
+# {
+#   ex_x = array(0, dim = c(3,4));
+#   ex_d = array(0, dim = c(3,3));  
+#   
+#   #slash
+#   ex_x[1,1] = 0; ex_x[1,2] = 1; ex_x[1,3] = 1; ex_x[1,4] = 0;
+#   ex_d[1,1] = 1; ex_d[1,2] = 0; ex_d[1,3] = 0;
+#   
+#   # backslash
+#   ex_x[2,1] = 1; ex_x[2,2] = 0; ex_x[2,3] = 0; ex_x[2,4] = 1;
+#   ex_d[2,1] = 0; ex_d[2,2] = 1; ex_d[2,3] = 0;
+#   
+#   # ar
+#   ex_x[3,1] = 1; ex_x[3,2] = 1; ex_x[3,3] = 0; ex_x[3,4] = 1;
+#   ex_d[3,1] = 0; ex_d[3,2] = 0; ex_d[3,3] = 1;
+#   
+#   return (list(ex_x, ex_d));
+# }
+
+# ex_data_x4 = function()
+# {
+#   ex_x = array(0, dim = c(5,4));
+#   ex_x[1,1] = 0; ex_x[1,2] = 1; ex_x[1,3] = 1; ex_x[1,4] = 0;
+#   ex_x[2,1] = 1; ex_x[2,2] = 0; ex_x[2,3] = 0; ex_x[2,4] = 1;
+#   ex_x[3,1] = 1; ex_x[3,2] = 1; ex_x[3,3] = 0; ex_x[3,4] = 1;
+#   ex_x[4,1] = 1; ex_x[4,2] = 1; ex_x[4,3] = 1; ex_x[4,4] = 1;
+#   ex_x[5,1] = 0; ex_x[5,2] = 1; ex_x[5,3] = 1; ex_x[5,4] = 1;
+#   
+#   return (ex_x);
+# }
+
+#check_point = function()
+#{        
+  #   if (steps %% 3 != 0)
+  #     return;
+  #   
+  #   print("----------------");
+  #   print("x = "); print(x);
+  #   print("y = "); print(y);
+  # 
+  #   res = readline('Continue or run example (y, n, num(1..3), p(plot weights))?');
+  #   learn = 1;
+  #   if (res == "n") {
+  #     return;
+  #   } else if (res == "1") {
+  #     x = ex_x[1,];
+  #     d = ex_d[1,];
+  #   } else if (res == "2") {
+  #     x = ex_x[2,];
+  #     d = ex_d[2,];
+  #   } else if (res == "3") {
+  #     x = ex_x[3,];
+  #     d = ex_d[3,];
+  #   } else if (res == "p") {
+  #     plot_weights(w[3,,]);
+  #   } else {
+  #     learn = 0;
+  #     x = c(1, 1, 1, 1);
+  #   }
+#}
